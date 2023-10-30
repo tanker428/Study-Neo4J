8c2c913777edfeb8252d483509c1489b3ed37885
tanker428 <130041984+tanker428@users.noreply.github.com>
2023-10-30 19:52:28 +0900

    relationship
    全て動かすことができた
    pandasとかを十二分に使えてないけどなんかやる

---
+++ py2neo-quick-start/test/relationship.py
@@ -0,0 +1,97 @@
+from py2neo import Graph, RelationshipMatcher, NodeMatcher
+from py2neo.integration import Table
+import pandas, sympy, numpy
+
+my_graph = Graph("bolt://localhost:7687", name = "try", password='vrwiki81')
+# my_graph.nodes.match()
+
+"""
+relationship
+"""
+
+matcher = NodeMatcher(my_graph)
+
+relationship_matcher = RelationshipMatcher(my_graph)
+relationship_matcher.match()
+
+my_graph.relationships.match()
+
+"""
+list all Tom Hanks movies
+
+cypher
+MATCH (tom:Person {name: "Tom Hanks"})-[:ACTED_IN]->(tomHanksMovies) RETURN tom,tomHanksMovies
+"""
+
+Tom = my_graph.nodes.match(name="Tom Hanks").first()
+Toms = my_graph.match(nodes=[Tom], r_type="ACTED_IN").all()
+print(f"Tom Hanks acted in movies: {Toms}\n")
+
+"""
+who directed "Cloud Atlas"? 
+
+cypher
+MATCH (cloudAtlas {title: "Cloud Atlas"})<-[:DIRECTED]-(directors) RETURN directors.name
+
+a pattern
+"""
+
+results = my_graph.run('MATCH (cloudAtlas {title: "Cloud Atlas"})<-[:DIRECTED]-(directors) RETURN directors.name')
+results = results.data()
+
+print(f"Cloud Atlas actors: {results}\n")
+
+"""
+b pattern
+"""
+cloudAtlas = matcher.match(title="Cloud Atlas").first()
+directors = my_graph.match(r_type="DIRECTED", nodes=(None, cloudAtlas)) # << see notes about use of nodes=() here
+for director in directors:
+     print(director.nodes[0]['name'])
+print("\n")
+
+
+"""
+Tom Hanks' co-actors
+
+cypher
+MATCH (tom:Person {name:"Tom Hanks"})-[:ACTED_IN]->(m)<-[:ACTED_IN]-(coActors) RETURN coActors.name
+"""
+
+results = my_graph.run('MATCH (tom:Person {name:"Tom Hanks"})-[:ACTED_IN]->(m)<-[:ACTED_IN]-(coActors) RETURN coActors.name')
+results = results.data()
+print(f"Tom Hanks' co-actors: {results}\n")
+
+"""
+How people are related to "Cloud Atlas"
+
+cypher
+MATCH (people:Person)-[relatedTo]-(:Movie {title: "Cloud Atlas"}) RETURN people.name, Type(relatedTo), relatedTo
+"""
+
+results = my_graph.run('MATCH (people:Person)-[relatedTo]-(:Movie {title: "Cloud Atlas"}) RETURN people.name, Type(relatedTo), relatedTo')
+result = results.data()
+print(f"{result}\n")
+
+# 分ければいける
+query_string = 'MATCH (people:Person)-[relatedTo]-(:Movie {title: "Cloud Atlas"}) RETURN people.name, Type(relatedTo), relatedTo'
+results_table = my_graph.run(query_string).to_table()
+print(results_table)
+
+# pythonを用いて出力をいじれる
+results = my_graph.run(query_string)
+
+# pandas
+result_data_frame = results.to_data_frame()
+print(result_data_frame)
+
+result_series = results.to_series()
+print(result_series)
+
+# sympy
+result_matrix = results.to_matrix()
+print(result_matrix)
+
+# numpy
+result_ndarray = results.to_ndarray()
+print(result_ndarray)
